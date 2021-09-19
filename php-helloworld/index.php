09400ca8fd9509024bcb01bd00cc34a92ed9acec
Omar Azzam <omar.azzam@ibm.com>
2021-09-19 02:12:15 -0400

    Updated index php file

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello again , World! php version is " . PHP_VERSION . "\n";
+print "Hello for the third time, World! php version is " . PHP_VERSION . "\n";
+
 ?>
